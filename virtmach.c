462a3db46626ec9ebf57e3c9e4cdfb04fd112a34
Dashi Cao <dscao999@gmail.com>
2019-11-12 20:34:05 +0800

    Implement op_equalverify

--- virtmach.c
+++ virtmach.c
@@ -2,40 +2,55 @@
 #include <stdlib.h>
 #include "loglog.h"
 #include "virtmach.h"
+#include "base64.h"
+#include "ecc_secp256k1.h"
 
-struct vmach *vmach_init(const unsigned char *sig, int len)
+#define VMACH_SCRATCH	8192
+#define VMACH_BUFLEN	2097152
+
+struct vmach *vmach_init(const unsigned char *dgst, int len)
 {
 	struct vmach *vm;
+	int i, chunk_len;
 
-	vm = mmap(NULL, 2097152, PROT_READ|PROT_WRITE,
+	if (len > 128)
+		return NULL;
+
+	chunk_len = VMACH_BUFLEN + VMACH_SCRATCH + sizeof(struct vmach);
+	vm = mmap(NULL, chunk_len, PROT_READ|PROT_WRITE,
 			MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 	if (vm) {
-		if (len > 128) {
+		vm->ripe = ripemd160_init();
+		if (!vm->ripe) {
 			free(vm);
 			vm = NULL;
 		} else {
-			vm->len = 2097152;
-			vm->top = 1024;
-			vm->siglen = len;
+			vm->chunk_len = chunk_len;
+			vm->top = VMACH_STACK_SIZE;
+			vm->buflen = VMACH_BUFLEN;
+			vm->scratch = vm->buf + vm->buflen;
 			vm->bufpos = 0;
-			memcpy(vm->sig, sig, len);
+			for (i = 0; i < VMACH_STACK_SIZE; i++)
+				vm->stack[i] = NULL;
+			vm->dgstlen = len;
+			memcpy(vm->dgst, dgst, len);
 		}
 	}
 	return vm;
 }
 
-int vmach_dupdata(struct vmach *vm)
+static int vmach_dupdata(struct vmach *vm)
 {
 	int spos, len;
 	unsigned char *ct, *nt;
 
-	spos = vm->top;
-	if (vmach_stack_empty(vm))
+	if (vmach_stack_empty(vm) || vmach_stack_full(vm))
 		return -ENOMEM;
+	spos = vm->top;
 	ct = vm->stack[spos];
 	nt = vm->buf + vm->bufpos;
 	len = *ct;
-	if (len + vm->bufpos + 1 > vm->len - sizeof(struct vmach))
+	if (len + vm->bufpos + 1 > vm->buflen)
 		return -ENOMEM;
 	*nt = *ct;
 	memcpy(nt+1, ct+1, len);
@@ -46,14 +61,14 @@ int vmach_dupdata(struct vmach *vm)
 	return 0;
 }
 
-int vmach_pushdata(struct vmach *vm, const unsigned char *opd, int len)
+static int vmach_pushdata(struct vmach *vm, const unsigned char *opd, int len)
 {
-	int spos = vm->top - 1;
+	int spos;
 	unsigned char *buf;
 
-	len &= 0x7f;
-	if (spos < 0 || vm->bufpos + len + 1 > vm->len - sizeof(struct vmach))
+	if (vmach_stack_full(vm) || vm->bufpos + len + 1 > vm->buflen)
 		return -ENOMEM;
+	spos = vm->top - 1;
 	buf = vm->buf + vm->bufpos;
 	*buf = len;
 	memcpy(buf+1, opd, len);
@@ -63,33 +78,131 @@ int vmach_pushdata(struct vmach *vm, const unsigned char *opd, int len)
 	return len;
 }
 
-int vmach_popdata(struct vmach *vm, unsigned char *buf, int buflen)
+static int vmach_popdata(struct vmach *vm, unsigned char *buf, int buflen)
 {
-	int spos = vm->top, len;
+	int spos, len;
 	unsigned char *ct;
 
 	if (vmach_stack_empty(vm))
 		return -ENOMEM;
+	spos = vm->top;
 	ct = vm->stack[spos];
 	len = *ct;
-	if (buflen < len)
-		return -ENOMEM;
-	memcpy(buf, ct+1, len);
+	memcpy(buf, ct+1, len > buflen? buflen:len);
+	vm->stack[spos] = NULL;
 	vm->top++;
 	vm->bufpos -= (len + 1);
 	return len;
 }
 
+static int vmach_push_bool(struct vmach *vm, int tf)
+{
+	int spos;
+	unsigned char *opv;
+
+	if (vmach_stack_full(vm))
+		return -ENOMEM;
+	spos = vm->top - 1;
+	opv = vm->buf + vm->bufpos;
+	*opv = 1;
+	*(opv+1) = tf;
+	vm->bufpos += 2;
+	vm->stack[spos] = opv;
+	vm->top = spos;
+	return 0;
+}
+
+static int vmach_op_equalverify(struct vmach *vm)
+{
+	unsigned char *opl, *opr;
+	int l_len, r_len, top, tf;
+
+	top = vm->top;
+	opl = vm->stack[top];
+	opr = vm->stack[top+1];
+	l_len = *opl;
+	r_len = *opr;
+
+	vm->stack[top] = NULL;
+	vm->stack[top+1] = NULL;
+	vm->top = top + 2;
+	vm->bufpos -= (r_len + l_len + 2);
+	if (l_len == r_len && memcmp(opl+1, opr+1, r_len) == 0)
+		tf = 0;
+	else 
+		tf = 1;
+	return tf;
+}
+static int vmach_ripemd160(struct vmach *vm)
+{
+	int spos, msglen;
+	unsigned char *msg, *ct;
+	struct ecc_key *ecckey;
+	char *keystr;
+
+	if (vmach_stack_empty(vm))
+		return -ENOMEM;
+	spos = vm->top;
+	ct = vm->stack[spos];
+	msglen = *ct;
+	msg = ct + 1;
+	keystr = vm->scratch;
+	ecckey = vm->scratch + msglen + 1;
+	memcpy(keystr, msg, msglen);
+	keystr[msglen] = 0;
+	ecc_key_import(ecckey, keystr);
+	ripemd160_dgst(vm->ripe, (void *)ecckey->px, ECCKEY_INT_LEN*8);
+	vm->bufpos -= (msglen + 1);
+	msglen = bignum2str_b64((char *)msg, vm->buflen - vm->bufpos,
+			vm->ripe->H, RIPEMD_LEN/4);
+	*ct = msglen + 1;
+	msg[msglen] = 0;
+	ripemd160_reset(vm->ripe);
+	vm->bufpos += (msglen + 2);
+	return 0;
+}
+
+int cmd_execute(struct vmach *vm, unsigned char cmd)
+{
+	int retv = 0;
+
+	switch(cmd) {
+	case OP_DUP:
+		retv = vmach_dupdata(vm);
+		break;
+	case OP_EQUALVERIFY:
+		retv = vmach_op_equalverify(vm);
+		break;
+	case OP_RIPEMD160:
+		retv = vmach_ripemd160(vm);
+		break;
+	case OP_CHECKSIG:
+		retv = vmach_checksig(vm);
+		break;
+	default:
+		retv = -1;
+	}
+	return retv;
+}
+
 int vmach_execute(struct vmach *vm, const unsigned char *script, int len)
 {
-/*	int retv = 0, pos = 0, len;
-	const unsigned char *scbyte;
+	int retv = 0, pos = 0;
+	const unsigned char *token;
 
-	scbyte = script + pos;
-	if ((*scbyte & 0x80)) {
-	} else {
-		len = *scbyte;
+	token = script;
+	do {
+		if ((*token & 0x80)) {
+			retv = cmd_execute(vm, *token);
+		} else {
+			len = *token;
+			retv = vmach_pushdata(vm, token+1, len);
+			pos += len + 1;
+			token += len + 1;
+		}
+	} while (pos < len && retv == 0);
+	if (retv != 0 || !vmach_stack_empty(vm))
+		retv = 1;
 
-	} */
-	return 0;
+	return retv;
 }
--- virtmach.h
+++ virtmach.h
@@ -1,36 +1,43 @@
 #ifndef VIRTMACH_DSCAO__
 #define VIRTMACH_DSCAO__
 #include <sys/mman.h>
+#include "ripemd160.h"
+#include "sha256.h"
 
+#define VMACH_STACK_SIZE	1024
 enum vcode {
-	OP_NOP = 0x81, OP_CHECKSIG = 0xac, OP_EQUAL = 0x87, OP_DUP = 0x82,
-	OP_POPDATA = 0x8d, OP_RIPEMD160 = 0xae
+	OP_NOP = 0x81, OP_CHECKSIG = 0xac, OP_EQUALVERIFY = 0x87,
+	OP_DUP = 0x82, OP_POPDATA = 0x8d, OP_RIPEMD160 = 0xae
 };
 
 struct vmach {
-	int len;
+	int chunk_len;
 	int top;
-	void *stack[1024];
-	int siglen, bufpos;
-	unsigned char sig[128];
+	struct ripemd160 *ripe;
+	void *stack[VMACH_STACK_SIZE];
+	int dgstlen, bufpos, buflen;
+	unsigned char dgst[128];
+	void *scratch;
 	unsigned char buf[0];
 };
 
-struct vmach *vmach_init(const unsigned char *sig, int len);
+struct vmach *vmach_init(const unsigned char *dgst, int len);
 
 static inline void vmach_exit(struct vmach *vm)
 {
-	if (vm)
-		munmap(vm, vm->len);
+	if (vm) {
+		ripemd160_exit(vm->ripe);
+		munmap(vm, vm->chunk_len);
+	}
 }
-
-int vmach_pushdata(struct vmach *vm, const unsigned char *opd, int len);
-int vmach_popdata(struct vmach *vm, unsigned char *buf, int buflen);
-int vmach_dupdata(struct vmach *vm);
 
 static inline int vmach_stack_empty(const struct vmach *vm)
 {
-	return vm->top == 1024;
+	return vm->top == VMACH_STACK_SIZE;
+}
+static inline int vmach_stack_full(const struct vmach *vm)
+{
+	return vm->top == 0;
 }
 
 int vmach_execute(struct vmach *vm, const unsigned char *script, int len);
